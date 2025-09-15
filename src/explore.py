b4f39c9da89ec31c8cbb5b0ad5639a100529a9fc
Kronk99 <zilean3312@gmail.com>
2025-09-15 02:15:21 -0600

    nueva division del dataset, por terciles, pendiente modificar el metodo de reparto entre training testing y validation con terciles

--- src/explore.py
+++ src/explore.py
@@ -12,6 +12,7 @@ df = pd.read_csv('dataset/Student_Performance.csv')
 #print(df.describe())
 ##seccion de histogramas para la exploracion numerica, x representa la variable independiente o caracterisitca, para este caso la y es la frecuencia o conteo
 #no el rendimiento academico segun se entiende
+"""
 df.hist(figsize=(10,8), bins=20)
 plt.suptitle('Histogramas de Variables Numéricas')
 plt.show()
@@ -77,3 +78,62 @@ print("Duplicados exactos (filas completas):", n_dup_total)
 dup_rows = df[df.duplicated(keep=False)].sort_values(by=list(df.columns))
 
 print(dup_rows.head(20).to_string())
+
+
+
+#----------------------------------------------------eliminar duplicados antes de separar la data--------------
+# eliminar duplicados exactos (mantener la primera ocurrencia), segun lo que dijo el profe en el grupo 
+df_clean = df.drop_duplicates(keep='first').reset_index(drop=True)
+
+#busqueda de casos atipicos, con la data eliminada y la tecnica vista de rangos intercuartilicos:
+def detect_outliers_iqr(series, k=1.5):
+    q1 = series.quantile(0.25)
+    q3 = series.quantile(0.75)
+    iqr = q3 - q1
+    lower = q1 - k * iqr
+    upper = q3 + k * iqr
+    return (series < lower) | (series > upper)
+
+# ejemplo para todas las numéricas
+outlier_flags = {}
+#para cada elemento en df_numeric , busque outliers
+for c in df_numeric:
+    flags = detect_outliers_iqr(df_clean[c].dropna())
+    outlier_flags[c] = flags.sum()
+    print(f"{c}: posibles outliers IQR = {flags.sum()}")
+"""
+ #----------------------------------------------------Division de la data stratified , strada = grupos de cuartiles--------------
+
+ #Como el dataset es de rendimiento academico , una opcion es crear los subgrupos en base a rendimiento academico bajo una medida predefinida
+ #por ejemplo 3 grupos, reprobados, rendimiento medio, rendimiento alto, sin embargo estas medidas tienen sesgo ya que dependen de "que es considerado como 
+ #rendimiento medio o rendimiento alto , es decir criterio personal"
+
+ # La manera más precisa de division : cuartiles (da mejores resultados en el modelo)
+
+#metodo para generar cuartiles, recomendado 3, al ser la data total de 9873 muestras
+def crear_strada(df,target,num_cuartiles):
+    #genera los cuartiles en el mismo dataframe df, df es el dataframe SIN REPETIDOS 
+    df['strata'] = pd.qcut(df[target], q=num_cuartiles, labels=False, duplicates='drop') #elimina duplicados en empates (dejar los empates o no?)
+    # fallback si quedaron NaN (caso raro)
+    if df['strata'].isnull().any():
+        print("se genero data NaN")
+    else:
+        df['strata'] = df['strata'].astype(int)
+    # reasignar labels contiguos (0..k-1) por si qcut redujo bins
+    unique = sorted(df['strata'].unique())
+    mapping = {old:new for new, old in enumerate(unique)}
+    df['strata'] = df['strata'].map(mapping).astype(int)
+    return df
+
+df_clean = df.drop_duplicates(keep='first').reset_index(drop=True)
+df_cuartiles =crear_strada(df_clean,'Performance Index',3) #me genera 3 cuartiles con muestras de  3291 cada una
+print(df_cuartiles['strata'].value_counts().sort_index()) #imprime las filas strata y su cantidad de elementos
+print(df_cuartiles[['Performance Index', 'strata']].head(20)) #imprime los primeros 20 elementos y el grupo percentil asociado
+print(df_cuartiles.groupby('strata')['Performance Index'].agg(['count','min','mean','max'])) #imprime minimos, maximos y mediana de cada cuartil 
+
+
+
+
+
+
+  
